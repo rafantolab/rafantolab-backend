c0d5707723d3fcbbc12b207897701948673d7145
nahidraz23 <nzamanraz@gmail.com>
2026-03-02 00:50:06 +0600

    fix: nodemailer setting updated

--- src/app/config/email.ts
+++ src/app/config/email.ts
@@ -4,6 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     service: 'gmail',
     auth: {
         user: process.env.GMAIL_USER,
