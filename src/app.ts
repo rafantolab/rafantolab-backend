1a36cf5dfec471bbdab3ea2f2d773444d713e398
nahidraz23 <nzamanraz@gmail.com>
2026-04-01 11:56:40 +0600

    chore: cors origin configuration updated

--- src/app.ts
+++ src/app.ts
@@ -3,34 +3,29 @@ import cors from "cors";
 import { LeadRoutes } from "./app/modules/lead/lead.route";
 
 const app = express();
-app.use(cors())
+
 const allowedOrigins = [
     "http://localhost:3000",
     "https://rafantolab.onrender.com",
     "https://www.rafantolab.com",
 ];
 
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like Postman)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin as string)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS",
-    );
-
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-
-    next();
-});
 
 app.use("/api/v1", LeadRoutes);
 
