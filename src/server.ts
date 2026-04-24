ed918b75a85ddf430921308fd83e5f9117d6ecef
nahidraz23 <nzamanraz@gmail.com>
2026-04-24 18:31:54 +0600

    fix: header added to all request

--- src/server.ts
+++ src/server.ts
@@ -22,5 +22,17 @@ const startServer = async() => {
     }
 }
 
+export default (req: any, res: any) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    return res.status(200).end();
+  }
+
+  return app(req, res);
+};
+
 startServer();
 
