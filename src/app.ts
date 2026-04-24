10f0eacfee8859842cc625717f2267ddc2afd88a
nahidraz23 <nzamanraz@gmail.com>
2026-04-24 17:20:03 +0600

    fix: cors origin updated

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(cors({
             callback(new Error("Not allowed by CORS"));
         }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
